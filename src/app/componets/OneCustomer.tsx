f43cc5e9b738dd807949565534e9c02c902d6ac2
RyuaUehara <u-ryua-2410@occ.co.jp>
2024-05-14 14:59:46 +0900

    ログイン画面にお客様の名前が出るようにしました

---
+++ src/app/componets/OneCustomer.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { CustomerType } from '@/app/api/customer/type';
+
+const OneCustomer = () => {
+  const [customer, setCustomer] = useState<CustomerType | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const id = window.location.pathname.split('/').pop(); // Get id from URL path
+
+  useEffect(() => {
+    const fetchCustomer = async () => {
+      if (id) {
+        setIsLoading(true);
+        try {
+          const res = await fetch(`/api/customer/${id}`);
+          const customerData = await res.json();
+          setCustomer(customerData);
+        } catch (error) {
+          console.error('Error fetching customer data:', error);
+        }
+        setIsLoading(false);
+      }
+    };
+    fetchCustomer();
+  }, [id]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="w-1/2 flex flex-col">
+      <div className="flex justify-between mb-5">
+        <p className="text-center font-bold text-3xl">Customer Details</p>
+      </div>
+      {customer && (
+        <div className="flex flex-col items-center justify-start">
+          <p>{JSON.stringify(customer)}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OneCustomer;
--- src/app/customer/login/[id]/page.tsx
+++ src/app/customer/login/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Link from "next/link";
+import OneCustomer from "@/app/componets/OneCustomer";
 
 export default function Home() {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <OneCustomer />
         <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           ログイン
         </a>
