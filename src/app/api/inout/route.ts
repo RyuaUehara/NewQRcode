a8ad1f53a54551c73ce5c6116bda682123f978b3
RyuaUehara <u-ryua-2410@occ.co.jp>
2024-05-16 16:13:27 +0900

    画面変更

--- src/app/api/inout/route.ts
+++ src/app/api/inout/route.ts
@@ -3,14 +3,18 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/PrismaClient";
 
 export const POST = async (req: Request, res: NextResponse) => {
-    const { helperid, helpername, customername } = await req.json();
-
+    const { helperID, helperName, customerName } = await req.json();
+    console.log(helperID,helperName,customerName)
     const new_visit = await prisma.visits.create({
         data: {
-            helperID: helperid,
-            helperName: helpername,
-            customerName: customername,
+            helperID: helperID,
+            helperName: helperName,
+            customerName: customerName,
         },
     });
     return NextResponse.json(new_visit);
+};
+
+export const PUT = async (req: Request,{ params }:{params: {id:string}}, res: NextResponse) => {
+  
 };
--- src/app/api/inout/type.ts
+++ src/app/api/inout/type.ts
@@ -1,7 +1,7 @@
 export type visitsType = {
   id: number;
-  helperid: string;
-  helpername: string;
-  customername: string;
+  helperID: string;
+  helperName: string;
+  customerName: string;
   work_status: number;
 };
