421d30cd3e0a69cdbd15335c2f5fc6fb396744d2
RyuaUehara <u-ryua-2410@occ.co.jp>
2024-05-17 16:51:46 +0900

    出退勤機能追加

--- src/app/api/inout/route.ts
+++ src/app/api/inout/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/PrismaClient";
 import { parse } from "path";
 import { error } from "console";
+import NewCustomer from "@/app/componets/NewCustomer";
+import { Newsreader } from "next/font/google";
 
 export const POST = async (req: Request, res: NextResponse) => {
   const { helperID, helperName, customerName } = await req.json();
@@ -17,7 +19,21 @@ export const POST = async (req: Request, res: NextResponse) => {
   return NextResponse.json(new_visit);
 };
 
-export const GET = async (req: Request, res: NextResponse) => {
+export const PUT = async (req: Request, res: NextResponse) => {
+  const { helperID, customerName } = await req.json();
+  const end_time = new Date();
+  const out_visit = await prisma.visits.updateMany({
+    data: { end_time },
+    where: {
+      helperID: helperID,
+      customerName: customerName,
+      end_time: null,
+    },
+  });
+  return NextResponse.json(out_visit);
+};
+
+/*export const GET = async (req: Request, res: NextResponse) => {
     const view_visits = await prisma.visits.findMany();
     return NextResponse.json(view_visits);
-};
+};*/
