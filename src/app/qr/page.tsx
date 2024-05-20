9e9f42c5050f37f2db7526c0f51f1cab11fda906
RyuaUehara <u-ryua-2410@occ.co.jp>
2024-05-20 14:13:00 +0900

    変更

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -11,6 +11,16 @@ const mainpage = () => {
             <div>
                 ホーム画面
             </div>
+            <div className="flex space-x-10">
+                <button onClick={() => setState(0)}>Page A</button>
+                <button onClick={() => setState(1)}>Page B</button>
+                <button onClick={() => setState(2)}>Page C</button>
+            </div>
+            <div>
+                {state === 0 && 0}
+                {state === 1 && 1}
+                {state === 2 && 2}
+            </div>
         </div>
     )
 }
