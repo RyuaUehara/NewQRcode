8cf32d52c49aa3ba6d251a561bdee010f8fa3c8d
TakaraHideto <t-hideto-2411@occ.co.jp>
2024-05-24 11:35:23 +0900

    a

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -8,6 +8,9 @@ import { useStaff } from "@/lib/utils/StaffProvider";
 import { visitType } from "../api/inout/type";
 import { FaCameraRetro } from "react-icons/fa";
  
+
+
+ 
 const MainPage = () => {
   const [state, setState] = useState(0);
   //const [staffname, setStaffname] = useState<staffType[]>([]);
@@ -43,6 +46,8 @@ const MainPage = () => {
     });
   };
  
+  // const notify = () => toast("データを入れました");
+ 
   const handlesubmitout = async () => {
     const response = await fetch("/api/inout", {
       method: "PUT",
@@ -62,15 +67,15 @@ const MainPage = () => {
  
   return (
     <div className='flex flex-col min-h-screen w-full items-center'>
-      <div className='bg-pink-300 sticky p-4 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
-        <h1 className='text-3xl  text-center font-bold  w-full'>メインページ</h1>
-      </div>
+       <header className=' bg-pink-300 p-4 sticky w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
+        <h1 className='text-3xl font-bold text-center'>メインページ画面</h1>
+      </header>
  
       {state === 0 && (
         <div className='bg-white w-full h-screen flex flex-col justify-center items-center content-center font-bold'>
           <div className='text-black text-2xl'>
-            <p >日付：{currentDate}</p>
-            <p>ヘルパー名：{staff}</p>
+            <p className='text-center'>日付：{currentDate}</p>
+            <p className='text-3xl'>ヘルパー名：{staff}</p>
           </div>
  
           <button
@@ -86,17 +91,17 @@ const MainPage = () => {
             カメラ起動
           </button>
  
-          <div className=' flex justify-center gap-20 '>
+          <div className=' flex justify-center gap-20 mt-10'>
             {" "}
             <Link
               href='/'
-              className='bg-blue-400 text-white p-4 rounded-md text-4xl font-bold hover:bg-blue-800 '
+              className='focus:outline-none text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 bg-blue-400 font-bold px-6 py-4 p-4 text-4xl rounded-lg mr-2'
             >
               戻る
             </Link>
             <button
               onClick={() => setState(2)}
-              className='bg-blue-400 text-white p-4 rounded-md text-4xl font-bold hover:bg-blue-800 '
+              className='focus:outline-none text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 bg-blue-400 font-bold px-6 py-4 p-4 text-4xl rounded-lg mr-2'
             >
               次へ
             </button>
@@ -111,54 +116,47 @@ const MainPage = () => {
             {" "}
             <Link
               href='/'
-              className='bg-blue-400 text-white font-bold px-6 py-4 rounded-lg hover:bg-blue-800 '
+              className='focus:outline-none text-white  bg-blue-400 hover:bg-blue-500 focus:ring-4 bg-blue-400 font-bold px-6 py-4 p-4 text-4xl rounded-lg mr-2'
             >
               戻る
             </Link>
             <button
               onClick={() => setState(2)}
-              className='bg-blue-400 text-white font-bold px-6 py-4 rounded-lg hover:bg-blue-800 '
+              className='focus:outline-none text-white  bg-blue-400 hover:bg-blue-500 focus:ring-4 bg-blue-400 font-bold px-6 py-4 p-4 text-4xl rounded-lg mr-2'
             >
               次へ
             </button>
           </div>
         )}
         {state === 2 && (
-          <div className='flex flex-col justify-between items-center h-screen'>
-            <div className='flex flex-col items-center'>
+          <div className='flex justify-center items-center h-screen'>
+            <div className='flex flex-col items-center justify-center'>
               {/* Display customer name */}
-              <div className='mb-6 text-2xl font-bold'>
+              <div className='mb-10 text-3xl font-bold'>
                 <p>利用者名：{customer}</p>
               </div>
  
-              <div className=''>
-                {/* Label for helperID */}
-                <label
-                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'
-                  htmlFor='helperID'
-                ></label>
-              </div>
- 
-              <div className='flex justify-center mb-8 gap-20'>
+              <div className='flex justify-center items-center gap-20 '>
                 {/* Button for entry */}
- 
-                <button className='focus:outline-none text-white bg-blue-400 hover:bg-blue-500 p-4 rounded-md text-4xl font-bold'>
+                <button
+                  
+                  className='focus:outline-none text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 bg-blue-400 font-bold px-6 py-4 p-4 text-4xl rounded-lg mr-2'
+                >
                   入出
                 </button>
+               
                 {/* Button for exit */}
-                <button className='focus:outline-none text-white bg-pink-400 hover:bg-pink-500 p-4 rounded-md text-4xl font-bold'>
+                <button className='focus:outline-none text-white bg-pink-300 hover:bg-pink-400 focus:ring-pink-300 font-bold px-6 py-4 p-4 text-4xl rounded-lg mr-2'>
                   退出
                 </button>
               </div>
- 
-              <hr />
             </div>
           </div>
         )}
       </div>
  
       {/* Footer */}
-      <footer className='sticky bottom-0 left-0 z-20 w-full p-4 bg-gray-200 border-t shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-300 dark:border-gray-600'>
+      <footer className='sticky bottom-0 left-0 z-20 w-full p-4 bg-gray-300  border-gray-200 shadow md:flex md:items-center md:justify-center md:p-6 dark:bg-gray-300 dark:border-gray-600'>
         <p className='text-2xl font-bold text-center'>
           ヘルパーステーション{" "}
           <span className='text-yellow-500 text-3xl'>OCC</span>
@@ -169,4 +167,3 @@ const MainPage = () => {
 };
  
 export default MainPage;
- 
